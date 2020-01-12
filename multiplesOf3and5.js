2229244a3ba2e6f8e13cf8c256ae11f6003f4701
Rhmatullah Darwish <rahmat.darwish@gmail.com>
2020-01-12 09:43:13 +0430

    Create multiplesOf3and5.js

---
+++ multiplesOf3and5.js
@@ -0,0 +1,10 @@
+function multiplesOf3and5(number) {
+  // Good luck!
+  var sum = 0;
+  for(let i=0; i<number; i++){
+    if(i%3 ==0 || i%5 == 0) sum += i;
+  }
+  return sum;
+}
+
+console.log(multiplesOf3and5(1000));
